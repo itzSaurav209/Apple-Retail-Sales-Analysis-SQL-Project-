87094f050e40e8aaef4b953ae21aa81281033dc9
itzSaurav209 <sauravshukla1103@gmail.com>
2026-06-30 23:16:15 +0530

    Added SQL business problem queries

--- Apple Retail Sales Analysis (SQL Project).sql
+++ Apple Retail Sales Analysis (SQL Project).sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS apple_sales;  /* as apple sales databse already exist*/
 USE apple_sales;
-
+SHOW TABLES;
 /* apple retail sales project-1M sales datasets */
 
 /*first we see the rows of all the table*/
@@ -22,9 +22,31 @@ CREATE INDEX sales_product_id ON sales(product_id(20));
 
 
 select *from sales where store_id="ST-14";
-/*ab ham store_id ki indexing karne jaa rhe hai /*/
+/*ab ham store_id ki indexing karne jaa rhe hai */
+
 CREATE INDEX sales_store_id ON sales(store_id(20)) ;
 
 
 /*****************BUSINESS PROBLEM*********************************/
+/*Ques 1)FInd the number of stores in each country */
+SELECT country,count(Store_Name)
+FROM stores
+group by country;
+
+/*Ques 2)calculate the total number of units sold by each store */
+SELECT store_id, SUM(quantity) 
+FROM sales
+GROUP BY store_id;
+
+/*identify how many sales occured in december 2023*/
+SELECT COUNT(sale_id) 
+FROM sales
+where sale_date BETWEEN 01-12-2023 AND 31-12-2023;
+
+/*determine how many stores have never had a warranty claim filed */
+
+SELECT COUNT(sale_id)
+FROM warranty
+WHERE sale_id!=claim_id;
+
 
